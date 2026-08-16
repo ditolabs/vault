2554b0f3f239af11291be699835c38761581f345
ShadowSoldiers <halodito@gmail.com>
2026-08-16 18:30:32 +0700

    update

--- app/src/main/java/com/ditolabs/pwvault/ui/components/BrutalTopBar.kt
+++ app/src/main/java/com/ditolabs/pwvault/ui/components/BrutalTopBar.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
